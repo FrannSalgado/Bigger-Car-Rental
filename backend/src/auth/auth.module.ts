4e15088b1f5036c03bbebbbee6e0413cf35a3df5
Francisco Salgado <Frannsalgado8@gmail.com>
2025-02-17 20:08:53 -0300

    feat: add new imports to auth.module.ts

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -6,13 +6,16 @@ import { JwtAuthGuard } from './jwt-auth.guard';
 import { JwtStrategy } from './jwt.strategy';
 import { PassportModule } from '@nestjs/passport';
 import {JwtModule} from "@nestjs/jwt";
+import {AuthService} from "./auth.service";
+import {UsersModule} from "../users/users.module";
+import {SignUpDto} from "./dto/sign-up.dto";
 
 
 @Module({
   imports: [ConfigModule,PassportModule, JwtModule.register({
     secret: process.env.JWT_SECRET,
-  })],
-  providers: [JwtStrategy, JwtAuthGuard,CognitoService],
+  }),UsersModule ],
+  providers: [JwtStrategy, JwtAuthGuard,CognitoService, AuthService, SignUpDto],
   controllers: [AuthController],
   exports: [JwtAuthGuard,CognitoService],
 })
