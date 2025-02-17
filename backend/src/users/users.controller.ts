44eb6453c3ae219fc2906e6306a298ea94617eed
Francisco Salgado <Frannsalgado8@gmail.com>
2025-02-17 20:59:02 -0300

    fix(user.controller): remove non-functional code

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,6 +1,5 @@
-import { Controller, Post, Body, Get, UseGuards, Request } from '@nestjs/common';
+import { Controller, Get, UseGuards, Request } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { User } from './user.entity';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { RolesGuard } from '../auth/roles.guard';
 import { Roles } from '../auth/roles.decorator';
@@ -11,13 +10,6 @@ export class UsersController {
     @UseGuards(JwtAuthGuard, RolesGuard)
 
 
-    @Post('register')
-    async registerUser(
-        @Body() body: { username: string; password: string; role?: string }
-    ): Promise<User> {
-        const { username, password, role } = body;
-        return this.usersService.createUser(username, password, role);
-    }
     @UseGuards(JwtAuthGuard, RolesGuard)
     @Roles('user', 'admin')
     @Get('profile')
