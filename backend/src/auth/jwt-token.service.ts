1b9e8204bd8760d6de21d0caad337da353ad8196
Francisco Salgado <Frannsalgado8@gmail.com>
2025-02-24 19:20:27 -0300

    add jwt-token.service.ts

---
+++ backend/src/auth/jwt-token.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class JwtTokenService {
+    constructor(private readonly jwtService: JwtService) {}
+
+    generateResetToken(email: string): string {
+        return this.jwtService.sign(
+            { email },
+            { secret: process.env.JWT_SECRET, expiresIn: '15m' }
+        );
+    }
+
+
+}
