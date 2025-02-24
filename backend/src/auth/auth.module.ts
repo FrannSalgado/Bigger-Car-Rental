0ac620dd145d6e7caefce86eb83737488de391ba
Francisco Salgado <Frannsalgado8@gmail.com>
2025-02-24 20:10:11 -0300

    feat auth.module.ts

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -9,13 +9,16 @@ import {JwtModule} from "@nestjs/jwt";
 import {AuthService} from "./auth.service";
 import {UsersModule} from "../users/users.module";
 import {SignUpDto} from "./dto/sign-up.dto";
+import { EmailModule } from '../email/email.module';
+import { BullModule } from '@nestjs/bull';
+import { JwtTokenService } from './jwt-token.service';
 
 
 @Module({
   imports: [ConfigModule,PassportModule, JwtModule.register({
     secret: process.env.JWT_SECRET,
-  }),UsersModule ],
-  providers: [JwtStrategy, JwtAuthGuard,CognitoService, AuthService, SignUpDto],
+  }),UsersModule,EmailModule,BullModule.registerQueue({ name: 'emailQueue' }), ],
+  providers: [JwtStrategy, JwtAuthGuard,CognitoService, AuthService, SignUpDto,JwtTokenService],
   controllers: [AuthController],
   exports: [JwtAuthGuard,CognitoService],
 })
